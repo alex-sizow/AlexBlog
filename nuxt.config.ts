26a21964bb5f9fe1285d9dd61e7632d68123562d
Alex <alex_sizow@outlook.com>
2023-05-15 23:38:39 +0300

    strapi template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	modules: ['@nuxtjs/strapi'],
+	strapi: {
+		url: 'http://localhost:1337',
+	},
 	vite: {
 		css: {
 			preprocessorOptions: {
