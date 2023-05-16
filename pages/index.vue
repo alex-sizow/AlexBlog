6f90f1d820b180254f941a143f1c154e084067f3
Alex <alex_sizow@outlook.com>
2023-05-16 19:11:41 +0300

    blog posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const createDate = (time: string) => {
+	const date = new Date(time);
+	const options: object = {
+		month: 'long',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		year: 'numeric',
+	};
+	const formatter = new Intl.DateTimeFormat('ru-RU', options);
+	return formatter.format(date);
+};
+
+const {
+	find,
+	findOne,
+	create,
+	update,
+	delete: remove,
+} = useStrapi();
+
+// Get all restaurants
+const posts: any = await find('posts');
+
+// Get one restaurant by id
+</script>
 
 <template>
 	<div class="main">
-		<div class="main__item">
+		<NuxtLink
+			:to="`posts/${post.id}'`"
+			v-for="post in posts.data"
+			:key="post.id"
+			class="main__item">
 			<img
 				class="main__item_img"
 				src="@/assets/image/wolf.webp"
 				alt="" />
-			<div class="main__item_title">title</div>
-		</div>
-		<div class="main__item">xcv</div>
-		<div class="main__item">xcv</div>
+			<span class="main__item_title">
+				{{ post.attributes.title }}
+			</span>
+			<div class="main__item_description">
+				{{ post.attributes.description }}
+			</div>
+			<div class="main__item_date">
+				{{ createDate(post.attributes.publishedAt) }}
+			</div>
+		</NuxtLink>
 	</div>
 </template>
 
@@ -22,9 +58,9 @@
 	&__item {
 		transition: 0.2s;
 		max-width: 250px;
-		min-height: 250px;
+		height: 250px;
 		width: 100%;
-
+		position: relative;
 		background: linear-gradient(145deg, #ffffff, #dfdede);
 
 		border-radius: 12px;
@@ -34,17 +70,42 @@
 			width: 100%;
 			object-fit: cover;
 			object-position: 0px -25px;
-			height: 58%;
-		}
-	}
-	&__item:hover {
-		transition: 0.25s;
-		scale: 1.01;
-		&_img {
-			height: 68%;
+			height: 150px;
+			filter: brightness(92%);
+			transition: 0.25s;
 		}
 		&_title {
-			color: red;
+			color: white;
+			position: absolute;
+			top: 124px;
+			left: 12px;
+			transition: 0.45s;
+		}
+		&_description {
+			position: relative;
+			padding: 10px;
+			font-size: 20px;
+		}
+		&_date {
+			position: absolute;
+			bottom: 10px;
+			right: 12px;
+			font-size: 10px;
+		}
+		&:hover {
+			transition: 0.35s;
+			scale: 1.01;
+			img {
+				transition: 0.38s;
+				object-position: 0px -32px;
+				filter: brightness(100%);
+			}
+			span {
+				transition: 0.45s;
+				left: 62px;
+			}
+			div {
+			}
 		}
 	}
 }
---
+++ pages/posts/[id].vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+	<div>dsfsd</div>
+</template>
+
+<style scoped></style>
