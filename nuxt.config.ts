b79f02037af02b8b4d9aa6c874eacd31f2e7a58a
Alex <alex_sizow@outlook.com>
2023-09-03 23:07:31 +0300

    Animation of route transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	experimental: {
+		viewTransition: true,
+	},
 	css: [
 		'@/assets/css/main.css',
 		'@/assets/css/color.css',
